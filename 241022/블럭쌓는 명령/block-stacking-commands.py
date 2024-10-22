69d4b12c454cae950ba5bc4a8fd16bdafcdb1868
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2024-10-22 13:32:13 +0900

    Title: 블럭쌓는 명령, Time: 283ms, Memory: 48MB, Status: Passed - CodeTree

--- 241022/블럭쌓는 명령/block-stacking-commands.py
+++ 241022/블럭쌓는 명령/block-stacking-commands.py
@@ -1,12 +1,18 @@
 N, K = map(int, input().split())
-steps = [0] * N
+steps = [0] * (N+1) # 인덱스 1부터 시작
+
+# 범위 시작점에 +1, 끝점 다음 위치에 -1
 for _ in range(K):
     a, b = map(int, input().split())
+    steps[a-1] += 1 # 시작점
+    steps[b] -= 1 # 끝점 다음 위치
 
-    for i in range(a-1, b):
-        steps[i] += 1
+# 누적 합 계산
+for i in range(1, N):
+    steps[i] += steps[i-1]
 
+steps = steps[:-1] # 마지막 원소 제거
 steps.sort()
-mid = (1+N)//2-1
+mid = (1+N)//2 - 1
 
 print(steps[mid])
