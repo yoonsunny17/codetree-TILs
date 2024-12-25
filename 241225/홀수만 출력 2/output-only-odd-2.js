9b436451cf25af1179ad76ebe55e2bc30764000b
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2024-12-25 21:22:14 +0900

    Title: 홀수만 출력 2, Time: 180ms, Memory: 15MB, Status: Passed - CodeTree

--- 241225/홀수만 출력 2/output-only-odd-2.js
+++ 241225/홀수만 출력 2/output-only-odd-2.js
@@ -3,6 +3,6 @@ input = fs.readFileSync(0).toString().split(' ')
 
 let [b, a] = input.map(Number)
 
-const arr = Array.from({length: b - a + 1}, (_, i) => i+1).filter(v => v%2 !== 0).sort((a, b) => b - a)
+const arr = Array.from({length: b - a + 1}, (_, i) => a+i).filter(v => v%2 !== 0).sort((a, b) => b - a)
 
 console.log(arr.join(' '))
