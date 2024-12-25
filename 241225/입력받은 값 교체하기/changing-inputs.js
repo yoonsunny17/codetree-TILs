dc8d3ec1cbe500d50ddb10a875a42a65edb72e7d
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2024-12-25 18:30:51 +0900

    Title: 입력받은 값 교체하기, Time: 184ms, Memory: 15MB, Status: Passed - CodeTree

--- 241225/입력받은 값 교체하기/changing-inputs.js
+++ 241225/입력받은 값 교체하기/changing-inputs.js
@@ -1,6 +1,6 @@
 fs = require('fs')
 input = fs.readFileSync(0).toString().split(' ')
-let [a, b] = [input[0], input[1]];
+let [a, b] = [Number(input[0]), Number(input[1])];
 [a, b] = [b, a]
 
 console.log(a, b)
