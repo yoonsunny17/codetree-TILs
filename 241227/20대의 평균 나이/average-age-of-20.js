f2a16fc2b483170eb8e8b326c947dc65bce44044
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2024-12-27 18:17:18 +0900

    Title: 20대의 평균 나이, Time: 204ms, Memory: 15MB, Status: Passed - CodeTree

--- 241227/20대의 평균 나이/average-age-of-20.js
+++ 241227/20대의 평균 나이/average-age-of-20.js
@@ -4,7 +4,7 @@ arr = fs.readFileSync(0).toString().trim().split('\n').map(Number)
 let [totalAge, cnt] = [0, 0]
 
 for (let i=0; i<=arr.length-1; i++) {
-    if (arr[i] >= 30) {
+    if (arr[i] >= 30 || arr[i] < 20) {
         break;
     }
 
