fb34c4b45b3b5067f9669ce62d197805519c1ae2
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-05 20:56:25 +0900

    Title: 나눗셈의 나머지, Time: 169ms, Memory: 15MB, Status: Passed - CodeTree

--- 250105/나눗셈의 나머지/remainder-of-division.js
+++ 250105/나눗셈의 나머지/remainder-of-division.js
@@ -3,15 +3,15 @@ input = fs.readFileSync(0).toString().trim().split(' ').map(Number)
 
 let [a, b] = input
 
-let arr = Array(500).fill(0)
+let arr = Array(10).fill(0)
 
 while (a > 1) {
-    a = Math.floor(a / b)
     arr[a%b]++
+    a = Math.floor(a/b)
 }
 
 let rlt = 0
-for (let i=0; i<500; i++) {
+for (let i=0; i<arr.length; i++) {
     if (arr[i] !== 0) {
         rlt += arr[i]**2
     }
