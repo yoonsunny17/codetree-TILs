3e093f494b9d1305c3c3a870290b612948c6d735
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-06 21:09:19 +0900

    Title: 특정 위치의 문자, Time: 170ms, Memory: 15MB, Status: Passed - CodeTree

--- 250106/특정 위치의 문자/char-in-specific-location.js
+++ 250106/특정 위치의 문자/char-in-specific-location.js
@@ -2,9 +2,9 @@ fs = require('fs')
 str = fs.readFileSync(0).toString().trim()
 
 let idx = -1;
-const arr = ['L', 'E', 'B', 'R', 'O'];
+const arr = ['L', 'E', 'B', 'R', 'O', 'S'];
 
-for (let i=0; i<arr.length; i++) {
+for (let i=0; i<6; i++) {
     if (arr[i] === str) {
         idx = i
     }
