941cc58b5dc26a6b4df3fe8ff33bab379719e8f9
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-07 10:24:10 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 182ms, Memory: 15MB, Status: Passed - CodeTree

--- 250107/중복되지 않는 정수 중 최대/max-of-unique-number.js
+++ 250107/중복되지 않는 정수 중 최대/max-of-unique-number.js
@@ -8,13 +8,19 @@ let cnt = 0
 const INT_MAX = Number.MIN_SAFE_INTEGER
 let maxVal = INT_MAX
 
+numbs.sort((a, b) => b-a)
+
 for (let i=0; i<n; i++) {
-    if (numbs[i] === maxVal) {
-        cnt++
-    }
-    else if (numbs[i] > maxVal) {
+    if (numbs[i] > maxVal) {
         maxVal = numbs[i]
         cnt = 1
+    } else if (numbs[i] === maxVal) {
+        cnt++
+    }
+
+    if (cnt > 1 && numbs[i] < maxVal) {
+        maxVal = numbs[i]
+        cnt=1
     }
 }
 
