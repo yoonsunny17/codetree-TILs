bdb0cc53897fcf4d88da1b8587551060129946a3
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-08 20:11:19 +0900

    Title: 두 배열의 곱, Time: 197ms, Memory: 15MB, Status: Passed - CodeTree

--- 250108/두 배열의 곱/multiple-of-two-arrays.js
+++ 250108/두 배열의 곱/multiple-of-two-arrays.js
@@ -1,22 +1,29 @@
 fs = require('fs')
 input = fs.readFileSync(0).toString().trim().split('\n')
 
-let arr1 = []
-let arr2 = []
+let arr1 = Array(3).fill(0).map(() => Array(3).fill(0))
+let arr2 = Array(3).fill(0).map(() => Array(3).fill(0))
 
 for (let i=0; i<3; i++) {
-    arr1.push(input[i].split(' ').map(Number))
-}
-
-for (let i=4; i<7; i++) {
-    arr2.push(input[i].split(' ').map(Number))
+    arr1[i] = input[i].split(' ').map(Number)
 }
 
 for (let i=0; i<3; i++) {
-    let str = ''
+    arr2[i] = input[i+4].split(' ').map(Number)
+}
+
+let arr3 = Array(3).fill(0).map(() => Array(3).fill(0))
+for (let i=0; i<3; i++) {
     for (let j=0; j<3; j++) {
-        str += `${arr1[i][j] * arr2[i][j]} `
+        arr3[i][j] = arr1[i][j] * arr2[i][j]
     }
+}
 
+for (let row of arr3) {
+    let str = ''
+    for (let col of row) {
+        str += `${col} `
+    }
+    
     console.log(str)
 }
