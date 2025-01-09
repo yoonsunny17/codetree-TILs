b1bd50984d709846efc8b8259f18fc97f230d483
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-09 23:49:01 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 187ms, Memory: 15MB, Status: Passed - CodeTree

--- 250109/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.js
+++ 250109/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.js
@@ -2,18 +2,17 @@ fs = require('fs')
 input = fs.readFileSync(0).toString().trim().split('\n')
 
 let n = Number(input[0])
-
-let arr = []
-for (let i=1; i<=n; i++) {
-    arr.push(input[i])
-}
-
 let alpha = input[n+1]
-let rlt = []
-for (let i=0; i<n; i++) {
-    if (arr[i][0] === alpha) {
-        rlt.push(arr[i].length)
+
+let lengthSum = 0
+let cnt = 0
+
+for (let i=1; i<=n; i++) {
+    if (input[i][0] === alpha) {
+        let len = input[i].length
+        lengthSum += len
+        cnt++
     }
 }
 
-console.log(rlt.length, ((rlt.reduce((total, curr) => total += curr, 0))/rlt.length).toFixed(2))
+console.log(cnt, (lengthSum / cnt).toFixed(2))
