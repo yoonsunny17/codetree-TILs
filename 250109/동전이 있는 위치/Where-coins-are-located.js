433a9ace2a4798a8b93e55a99aa526be01e02cd8
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-09 23:09:33 +0900

    Title: 동전이 있는 위치, Time: 168ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250109/동전이 있는 위치/Where-coins-are-located.js
@@ -0,0 +1,23 @@
+fs = require('fs')
+input = fs.readFileSync(0).toString().trim().split('\n')
+
+let [n, m] = input[0].split(' ').map(Number)
+
+let arr = Array(n).fill(0).map(() => Array(n).fill(0))
+for (let i=1; i<m+1; i++) {
+    let [r, c] = input[i].split(' ').map(Number)
+
+    for (let i=0; i<n; i++) {
+        for (let j=0; j<n; j++) {
+            arr[r-1][c-1] = 1
+        }
+    }
+}
+
+for (let row of arr) {
+    let str = ''
+    for (let elem of row) {
+        str += `${elem} `
+    }
+    console.log(str)
+}
