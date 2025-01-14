b23d1a2820c71e2f61aea14af8cdecde1e643fb3
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-14 15:25:00 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 185ms, Memory: 15MB, Status: Passed - CodeTree

--- 250114/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.js
+++ 250114/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.js
@@ -4,16 +4,24 @@ const input = fs.readFileSync(0).toString().trim().split('\n')
 let n = Number(input[0])
 let numbs = input[1].split(' ').map(Number)
 
-function solution(arr, idx=0, currVal=1, rlt=1) {
-    if (idx === n) {
-        return rlt
-    }
-
-    if ((rlt % currVal === 0) && (rlt % arr[idx] === 0)) {
-        return solution(arr, idx+1, rlt, rlt )
-    } else {
-        return solution(arr, idx, currVal, rlt+1)
-    }
+// 최대공약수 구하기
+function gcd(a, b) {
+    if (b === 0) return a;
+    return gcd(b, a%b);
 }
 
-console.log(solution(numbs))
+// 최소공배수 구하기
+function lcm(a, b) {
+    return (a*b) / gcd(a, b);
+}
+
+// 재귀함수
+function solution(arr, idx=0, currLcm=1) {
+    if (idx === arr.length) {
+        return currLcm;
+    }
+
+    return solution(arr, idx+1, lcm(currLcm, arr[idx]))
+}
+
+console.log(solution(numbs));
