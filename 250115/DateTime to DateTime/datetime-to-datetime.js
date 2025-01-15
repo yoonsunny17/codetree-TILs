df55930b657ff41ad6eb633bec31e1ec6c08d129
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-15 14:34:28 +0900

    Title: DateTime to DateTime, Time: 170ms, Memory: 15MB, Status: Passed - CodeTree

--- 250115/DateTime to DateTime/datetime-to-datetime.js
+++ 250115/DateTime to DateTime/datetime-to-datetime.js
@@ -1,11 +1,3 @@
-function isValid() {
-    if (a < 11) return false
-    if (b < 11) return false
-    if (c < 11) return false
-
-    return true
-}
-
 const fs = require('fs')
 const input = fs.readFileSync(0).toString().trim().split(' ').map(Number)
 
@@ -14,25 +6,12 @@ let [a, b, c] = input
 let cnt = 0
 let date = 11, hour = 11, min = 11
 
-if (isValid()) {
-    while (true) {
-        if (date === a && hour === b && min === c) {
-            break;
-        }
+let start = date * 24 * 60 + hour * 60 + min
+let end = a * 24 * 60 + b * 60 + c
 
-        cnt++
-        min++
-
-        if (min === 60) {
-            hour++
-            min = 0
-        }
-
-        if (hour === 24) {
-            date++
-            hour = 0
-        }
-    }
+if (start > end) {
+    console.log('-1')
+} else {
+    cnt = end - start
+    console.log(cnt)
 }
-
-console.log(isValid() ? cnt : "-1")
