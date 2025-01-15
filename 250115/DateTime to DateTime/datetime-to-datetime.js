fefd3bb09161440186bb3314419c8bc8b8ccf3b3
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-15 14:31:49 +0900

    Title: DateTime to DateTime, Time: 165ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250115/DateTime to DateTime/datetime-to-datetime.js
+++ 250115/DateTime to DateTime/datetime-to-datetime.js
@@ -1,3 +1,11 @@
+function isValid() {
+    if (a < 11) return false
+    if (b < 11) return false
+    if (c < 11) return false
+
+    return true
+}
+
 const fs = require('fs')
 const input = fs.readFileSync(0).toString().trim().split(' ').map(Number)
 
@@ -5,23 +13,26 @@ let [a, b, c] = input
 
 let cnt = 0
 let date = 11, hour = 11, min = 11
-while (true) {
-    if (date === a && hour === b && min === c) {
-        break;
-    }
 
-    cnt++
-    min++
+if (isValid()) {
+    while (true) {
+        if (date === a && hour === b && min === c) {
+            break;
+        }
 
-    if (min === 60) {
-        hour++
-        min = 0
-    }
+        cnt++
+        min++
 
-    if (hour === 24) {
-        date++
-        hour = 0
+        if (min === 60) {
+            hour++
+            min = 0
+        }
+
+        if (hour === 24) {
+            date++
+            hour = 0
+        }
     }
 }
 
-console.log(cnt)
+console.log(isValid(a, b, c) ? cnt : "-1")
