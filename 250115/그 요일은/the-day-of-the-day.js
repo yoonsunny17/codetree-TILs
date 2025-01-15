ab1db516867c174f706b74c94c64c7802ad17844
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-15 21:18:36 +0900

    Title: 그 요일은, Time: 172ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250115/그 요일은/the-day-of-the-day.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [m1, d1, m2, d2] = input[0].split(' ').map(Number);
+const a = input[1];
+
+function calcDays(m, d) {
+    const daysOfMonth = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    let totalDays = 0;
+
+    // 1월부터 m-1월까지
+    for (let i=0; i<m; i++) {
+        totalDays += daysOfMonth[i];
+    }
+
+    // m월
+    totalDays += d;
+
+    return totalDays;
+}
+
+// 두 날짜 간격
+const diff = calcDays(m2, d2) - calcDays(m1, d1)
+console.log(diff % 7 >= 5 ? parseInt(diff / 7) + 1 : parseInt(diff / 7))
