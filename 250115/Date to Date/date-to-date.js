b44d4c8f1013a8a40a66f04833d08bb5ba86739e
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-15 14:13:03 +0900

    Title: Date to Date, Time: 170ms, Memory: 15MB, Status: Passed - CodeTree

--- 250115/Date to Date/date-to-date.js
+++ 250115/Date to Date/date-to-date.js
@@ -3,27 +3,23 @@ const input = fs.readFileSync(0).toString().trim().split(' ').map(Number)
 
 let [m1, d1, m2, d2] = input
 
-let num_of_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+let num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
+let month = m1, day = d1
+let cnt = 0
 
-function calc(m1, d1, m2, d2) {
-    let cnt = 0
-    let month = m1, day = d1
-    while (true) {
-        if (month === m2 && day === d2) {
-            break;
-        }
-
-        cnt++
-        day++
-
-        if (day > num_of_days[month]) {
-            month++
-            day = 1
-        }
+while (true) {
+    if (month === m2 && day === d2) {
+        break;
     }
 
-    return cnt
+    cnt++
+    day++
+
+    if (day > num_of_days[month]) {
+        month++
+        day = 1
+    }
 }
 
-console.log(calc(m1, d1, m2, d2))
+console.log(cnt+1)
