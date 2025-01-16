d8f8468b55ee38eb58795942eb10bdea653a28c1
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-16 23:07:16 +0900

    Title: 왔다 갔던 구역 2, Time: 173ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250116/왔다 갔던 구역 2/area-been-to-and-from2.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+let arr = Array(2000).fill(0);
+
+let point = 1000; // 음수값 조정 
+for (let i=0; i<n; i++) {
+    const [x, command] = input[i+1].split(' ');
+
+    if (command === 'R') {
+        for (let i=point; i<point+Number(x); i++) {
+            arr[i]++
+        }
+        point += Number(x);
+    } else {
+        for (let i=point; i>point-Number(x); i--) {
+            arr[i]++
+        }
+        point -= Number(x);
+    }
+};
+
+let cnt = 0;
+for (let i of arr) {
+    if (i >= 2) cnt++;
+}
+
+console.log(cnt);
