8c2f66fcc0fe2f0b2d5ce72ef60533141e8e52f8
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-19 13:41:38 +0900

    Title: 연속되는 수 4, Time: 179ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250119/연속되는 수 4/continuous-number4.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const numbs = input.slice(1);
+
+let cnt = 0;
+let ans = 0;
+for (let i=0; i<n; i++) {
+    if (i >= 1 && numbs[i] > numbs[i-1]) {
+        cnt++;
+    } else {
+        cnt = 1;
+    }
+
+    ans = Math.max(ans, cnt);
+}
+
+console.log(ans);
