1a368f153615e465f75a6b2bfe370c9888a1b5a8
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-19 17:51:17 +0900

    Title: 벌금은 누구에게, Time: 187ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250119/벌금은 누구에게/who-will-pay.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m, k] = input[0].split(' ').map(Number);
+let cnt = Array(n+1).fill(0);
+
+let ans = -1;
+// m개의 명령을 순차적으로 실행
+for (let i=0; i<m; i++) {
+    let command = Number(input[i+1]);
+
+    cnt[command] += 1;
+
+    if (cnt[command] === k) {
+        ans = command;
+    };
+}
+
+console.log(ans);
