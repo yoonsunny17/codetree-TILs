1e88db85427f81ab017038d2d2206e08e640e1eb
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-19 14:25:19 +0900

    Title: 만나는 그 순간, Time: 195ms, Memory: 31MB, Status: Wrong Answer - CodeTree

--- 250119/만나는 그 순간/the-moment-we-meet.js
+++ 250119/만나는 그 순간/the-moment-we-meet.js
@@ -3,9 +3,10 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [n, m] = input[0].split(' ').map(Number);
 
+let MAX_TIME = 1000000;
 // 1초부터 1000초까지 이동한 거리 체크해줄 리스트
-let lstA = Array(1000001).fill(0);
-let lstB = Array(1000001).fill(0);
+let lstA = Array(MAX_TIME + 1).fill(0);
+let lstB = Array(MAX_TIME + 1).fill(0);
 
 let idxA = 0;
 let idxB = 0;
@@ -42,9 +43,12 @@ for (let i=0; i<m; i++) {
     idxB = idxB+Number(t);
 }
 
-for (let i=1; i<1000001; i++) {
+let ans = -1;
+for (let i=1; i<MAX_TIME+1; i++) {
     if (lstA[i] === lstB[i]) {
-        console.log(i);
+        ans = i;
         break;
     }
 }
+
+console.log(ans);
