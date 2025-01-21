b8c395e2207023dd80b8cf12163e3a6ba9746317
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-21 23:15:21 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 199ms, Memory: 15MB, Status: Passed - CodeTree

--- 250121/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.js
+++ 250121/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.js
@@ -2,28 +2,23 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const N = Number(input.shift());
-let points = input.map((v) => v.split(' ').map(Number));
+const points = input.map((v) => v.split(' ').map(Number));
 
-let skipIdx = 1; // 건너 뛸 지점 인덱스
-let [sr, sc] = points[0]; // 출발 위치 초기화
 let minDist = Infinity; // 최소 거리 받을 변수 초기화
 
-// 맨 끝 지점 전까지 확인해보기
-while (skipIdx < N-1) {
-    let cnt = 0;
-    for (let i=1; i<N; i++) {
-        if (i === skipIdx) {
-            continue;
-        } else {
-            cnt += Math.abs(sr-points[i][0]) + Math.abs(sc-points[i][1]);
-            sr = points[i][0], sc = points[i][1];
-        }
+// i번째 체크포인트 건너뛰기
+for (let i=1; i<N-1; i++) {
+    let prevIdx = 0; // 이전에 있었던 곳의 인덱스
+    let cnt = 0; // 각 케이스마다 이동거리
+
+    for (let j=1; j<N; j++) {
+        if (j === i) continue; // 건너뛸 곳은 건너뛰기
+
+        cnt += Math.abs(points[prevIdx][0] - points[j][0]) + Math.abs(points[prevIdx][1] - points[j][1]);
+        prevIdx = j;
     }
 
     minDist = Math.min(minDist, cnt);
-
-    skipIdx++;
-    sr = 0, sc = 0;
 }
 
 console.log(minDist);
