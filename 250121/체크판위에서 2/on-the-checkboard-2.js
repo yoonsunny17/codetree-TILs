02e2e186ee1891ffe344750099ccec80591af4d8
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-21 21:37:23 +0900

    Title: 체크판위에서 2, Time: 161ms, Memory: 15MB, Status: Passed - CodeTree

--- 250121/체크판위에서 2/on-the-checkboard-2.js
+++ 250121/체크판위에서 2/on-the-checkboard-2.js
@@ -8,20 +8,19 @@ for (let i=0; i<R; i++) {
     matrix.push(input[i].split(' '));
 }
 
-// 초기 위치 설정
-let [r, c, curr] = [0, 0, matrix[0][0]];
-
+// 가능한 경우의 수 세어줄 변수
 let cnt = 0;
 // step1. 첫번째 점프에 도달하는 위치
 for (let i=1; i<R-1; i++) {
     for (let j=1; j<C-1; j++) {
-        if (i > r && j > c && matrix[i][j] !== curr) {
-            r = i, c = j, curr = matrix[i][j];
+        // 현재 칸의 색깔이 초기 위치의 색깔과 다른 경우만 점프 가능
+        if (matrix[i][j] !== matrix[0][0]) {
 
             // step2. 두번째 점프에 도달하는 위치   
             for (let k=i+1; k<R-1; k++) {
                 for (let l=j+1; l<C-1; l++) {
-                    if (k > r && l > c && matrix[k][l] !== curr) {
+                    // 현재 칸의 색깔이 이전 칸의 색깔과, 마지막 칸의 색깔과도 다른 경우 점프 가능
+                    if (matrix[k][l] !== matrix[i][j] && matrix[k][l] !== matrix[R-1][C-1]) {
                         cnt++;
                     }
                 }
