4cc55b64f7f383a3b41079fc1f15f90fd52cb29f
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-22 22:12:56 +0900

    Title: 최고의 13위치 2, Time: 178ms, Memory: 15MB, Status: Passed - CodeTree

--- 250122/최고의 13위치 2/best-place-of-13-2.js
+++ 250122/최고의 13위치 2/best-place-of-13-2.js
@@ -4,42 +4,28 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input.shift());
 const matrix = Array.from({length: n}, (_, i) => input[i].split(' ').map(Number));
 
-let maxVal = 0;
+// step1. 첫번째 격자를 놓는다
+let maxCnt = 0;
 for (let i=0; i<n; i++) {
-    let firstCheck = [];
-    let firstMax = 0;
+    // 격자 벗어나지 않는 범위 내에서 잡는다
     for (let j=0; j<n-2; j++) {
-        firstCheck = matrix[i].slice(j, j+3);
-
-        // 첫번째 검사 시 나올 수 있는 동전의 최댓값
-        if (firstCheck.reduce((total, curr) => total + curr, 0) > firstMax) {
-            firstMax = firstCheck.reduce((total, curr) => total + curr, 0);
-        }
-
+        // 두번째 격자를 놓는다
         for (let k=0; k<n; k++) {
-            let secondCheck = [];
-            let secondMax = 0;
-            if (k === i) {
-                for (let l=j+3; l<n-2; l++) {
-                    secondCheck = matrix[k].slice(l, l+3);
+            // 격자 벗어나지 않는 범위 내에서 잡는다
+            for (let l=0; l<n-2; l++) {
+                // step2. 두 격자가 겹치는 경우에 카운팅하지 않는다
+                if (i === k && Math.abs(j-l) <= 2) {
+                    continue;
                 }
-            } else {
-                for (let l=0; l<n-2; l++) {
-                    secondCheck = matrix[k].slice(l, l+3);
-                }
-            }
 
-            // 두번째 검사 시 나올 수 있는 동전의 최댓값
-            if (secondCheck.reduce((total, curr) => total + curr, 0) > secondMax) {
-                secondMax = secondCheck.reduce((total, curr) => total + curr, 0);
-            }
+                // step3. 두 격자가 겹치지 않는 경우에 동전의 개수를 세어주고, 최댓값을 갱신한다.
+                const coin1 = matrix[i][j] + matrix[i][j+1] + matrix[i][j+2];
+                const coin2 = matrix[k][l] + matrix[k][l+1] + matrix[k][l+2];
 
-            // 두 번의 검사에서 나오는 최댓값 갱신
-            if (maxVal < firstMax + secondMax) {
-                maxVal = firstMax + secondMax;
+                maxCnt = Math.max(maxCnt, coin1 + coin2);
             }
         }
     }
 }
 
-console.log(maxVal);
+console.log(maxCnt);
