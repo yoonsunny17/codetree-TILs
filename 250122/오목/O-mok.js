3590090b130226448a560dca598b396cd478c7fb
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-22 19:56:51 +0900

    Title: 오목, Time: 179ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250122/오목/O-mok.js
@@ -0,0 +1,107 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const matrix = Array.from({length: 19}, (_, i) => input[i].split(' ').map(Number));
+
+// 가로로 확인
+const garoCheck = () => {
+    for (let i=0; i<19; i++) {
+        for (let j=0; j<=14; j++) {
+            let slice = matrix[i].slice(j, j+5);
+
+            let [color, cnt] = [0, 0];
+
+            slice.forEach((v) => {
+                if (v === 1) {
+                    color--;
+                    cnt++;
+                } else if (v === 2) {
+                    color++;
+                    cnt++;
+                } else {
+                    color = 0;
+                    cnt = 0;
+                }
+            })
+
+            if ((color === 5 || color === -5) && cnt === 5) {
+                winnerColor = color;
+                r = i+1, c = j+3;
+                return;
+            }
+        }
+    }
+}
+
+// 세로로 확인
+const seroCheck = () => {
+    for (let j=0; j<19; j++) {
+        for (let i=0; i<=14; i++) {
+            const slice = matrix.slice(i, i+5).map(row => row[j]);
+
+            let [color, cnt] = [0, 0];
+
+            slice.forEach((v) => {
+                if (v === 1) {
+                    color--;
+                    cnt++;
+                } else if (v === 2) {
+                    color++;
+                    cnt++;
+                } else {
+                    color = 0;
+                    cnt = 0;
+                }
+            })
+
+            if ((color === 5 || color === -5) && cnt === 5) {
+                winnerColor = color;
+                r = i+1, c = j+3;
+                return;
+            }
+        }
+    }
+
+}
+
+// 대각선으로 확인
+const diagonalCheck = () => {
+    for (let i=0; i<=14; i++) {
+        for (let j=0; j<=14; j++) {
+            const slice = [
+                matrix[i][j], matrix[i+1][j+1], matrix[i+2][j+2], matrix[i+3][j+3], matrix[i+4][j+4]
+            ]
+
+            let [color, cnt] = [0, 0];
+
+            slice.forEach((v) => {
+                if (v === 1) {
+                    color--;
+                    cnt++;
+                } else if (v === 2) {
+                    color++;
+                    cnt++;
+                } else {
+                    color = 0;
+                    cnt = 0;
+                }
+            })
+
+            if ((color === 5 || color === -5) && cnt === 5) {
+                winnerColor = color;
+                r = i+1, c = j+3;
+                return;
+            }
+        }
+    }
+}
+
+// 검은돌 발견 시 -1 씩, 흰돌 발견 시 +1 씩
+let [winnerColor, r, c] = [0, 0, 0];
+
+garoCheck();
+seroCheck();
+diagonalCheck();
+
+console.log(winnerColor === 5 ? 2 : 1)
+console.log(r, c);
