46f5401086c1d259ed76a20d0c334ee45aac3f17
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-22 22:03:19 +0900

    Title: 최고의 13위치 2, Time: 182ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 250122/최고의 13위치 2/best-place-of-13-2.js
+++ 250122/최고의 13위치 2/best-place-of-13-2.js
@@ -4,46 +4,42 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input.shift());
 const matrix = Array.from({length: n}, (_, i) => input[i].split(' ').map(Number));
 
+let maxVal = 0;
 for (let i=0; i<n; i++) {
-    let cntCoins = 0;
+    let firstCheck = [];
+    let firstMax = 0;
     for (let j=0; j<n-2; j++) {
-        console.log("??", matrix[i].slice(j, j+3).reduce((total, curr) => total += curr, 0))
-        cntCoins += matrix[i].slice(j, j+3).reduce((total, curr) => total += curr, 0);
-        let curR = i;
-        let curC = j;
+        firstCheck = matrix[i].slice(j, j+3);
+
+        // 첫번째 검사 시 나올 수 있는 동전의 최댓값
+        if (firstCheck.reduce((total, curr) => total + curr, 0) > firstMax) {
+            firstMax = firstCheck.reduce((total, curr) => total + curr, 0);
+        }
+
         for (let k=0; k<n; k++) {
-            if (k === curR) {
-                // 다른 격자와 같은 행을 체크하는 경우
-                for (let l=curC+3; l<n-2; l++) {
-                    console.log('second', matrix[k].slice(l, l+3).reduce((total, curr) => total += curr, 0))
-                    cntCoins += matrix[k].slice(l, l+3).reduce((total, curr) => total += curr, 0);
+            let secondCheck = [];
+            let secondMax = 0;
+            if (k === i) {
+                for (let l=j+3; l<n-2; l++) {
+                    secondCheck = matrix[k].slice(l, l+3);
                 }
             } else {
-                // 다른 격자와 다른 행을 체크하는 경우
                 for (let l=0; l<n-2; l++) {
-                    console.log('second', matrix[k].slice(l, l+3).reduce((total, curr) => total += curr, 0))
-
-                    cntCoins += matrix[k].slice(l, l+3).reduce((total, curr) => total += curr, 0);
+                    secondCheck = matrix[k].slice(l, l+3);
                 }
             }
-        }
-        console.log(cntCoins)
-    }
 
+            // 두번째 검사 시 나올 수 있는 동전의 최댓값
+            if (secondCheck.reduce((total, curr) => total + curr, 0) > secondMax) {
+                secondMax = secondCheck.reduce((total, curr) => total + curr, 0);
+            }
+
+            // 두 번의 검사에서 나오는 최댓값 갱신
+            if (maxVal < firstMax + secondMax) {
+                maxVal = firstMax + secondMax;
+            }
+        }
+    }
 }
 
-// for (let i=0; i<n; i++) {
-//     for (let j=0; j<n-2; j++) {
-//         let curR, curC = i, j;
-
-//         for (let k=0; k<n; k++) {
-//             for (let l=0; l<n-2; l++) {
-//                 if (k === curR) {
-//                     console.log(matrix[i].slice(curC, curC+3));
-//                 } else {
-//                     console.log(matrix[i].slice(l, l+3));
-//                 }
-//             }
-//         }
-//     }
-// }
+console.log(maxVal);
