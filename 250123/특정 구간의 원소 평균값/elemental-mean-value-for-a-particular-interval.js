f27f6b56e98b7bd081381651de2dc043fda26411
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-23 19:46:40 +0900

    Title: 특정 구간의 원소 평균값, Time: 183ms, Memory: 15MB, Status: Passed - CodeTree

--- 250123/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.js
+++ 250123/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.js
@@ -4,7 +4,6 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const numbs = input[1].split(' ').map(Number);
 
-let cntArr = [];
 let cnt = 0;
 for (let i=0; i<n; i++) {
     for (let j=i; j<n; j++) {
@@ -13,9 +12,17 @@ for (let i=0; i<n; i++) {
             sumVal += numbs[k];
         }
         
-        if (numbs.includes(sumVal / (j-i+1))) {
-            cnt++;
+        let avg = sumVal / (j-i+1);
+
+        // 방금 확인한 구간 내에 avg랑 동일한 숫자가 있으면 cnt 해줘! (전체 구간 아님)
+        let flag = false;
+        for (let k=i; k<j+1; k++) {
+            if (numbs[k] === avg) {
+                flag = true;
+            }
         }
+
+        if (flag) cnt++;
     }
 }
 
