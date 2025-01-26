8376e3d79210b9b581c128bd6e9c55f7f55d203a
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-26 22:43:22 +0900

    Title: 스승의 은혜 2, Time: 166ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250126/스승의 은혜 2/the-grace-form-teacher-2.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, b] = input.shift().split(' ').map(Number);
+const prices = Array.from({length: n}, (_, i) => Number(input[i]));
+
+let maxVal = 0; // 가능한 최대 인원 출력할 변수
+
+// 할인받을 선물 하나를 고른다
+for (let i=0; i<n; i++) {
+    let changedPrices = [];
+    for (let j=0; j<n; j++) {
+        if (i === j) {
+            changedPrices.push(prices[j]/2);
+        } else {
+            changedPrices.push(prices[j]);
+        }
+    }
+
+    // 가격을 오름차순 정렬한 뒤, 예산 안에서 몇명 살 수 있는지 확인한다
+    changedPrices.sort((a, b) => a - b);
+
+    let cnt = 0; // 최대 몇명까지 사줄 수 있는지 세어줄 변수
+    let calc = 0;
+    for (let c of changedPrices) {
+        calc += c;
+
+        if (calc >= b) {
+            break;
+        } else {
+            cnt++;
+        }
+    }
+
+    maxVal = Math.max(maxVal, cnt);    
+}
+
+console.log(maxVal);
