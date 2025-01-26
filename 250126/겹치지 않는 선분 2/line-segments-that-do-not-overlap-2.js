9851426cb4c25d3834a0374715b8ceffc1554959
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-26 21:36:27 +0900

    Title: 겹치지 않는 선분 2, Time: 167ms, Memory: 15MB, Status: Passed - CodeTree

--- 250126/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.js
+++ 250126/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.js
@@ -6,36 +6,24 @@ const infos = Array.from({length: n}, (_, i) => input[i].split(' ').map(Number))
 
 let rlt = 0;
 
-let idx = 0;
-while (idx < n) {
-    let flag = false; // 겹쳐지는지 확인
+// 하나의 선을 기준으로 삼아서, 겹치는지 확인해본다
+for (let i=0; i<n; i++) {
+    let flag = false;
 
-    // 기준이 되는 선을 선택한다
-    let baseX1 = 0, baseX2 = 0;
-    for (let i=0; i<n; i++) {
-        if (i === idx) {
-            baseX1 = infos[i][0];
-            baseX2 = infos[i][1];
-        }
-    }
-    
-    // 해당 선에 대해서, 다른 선분들의 범위가 어떻게 되는지 확인한다
     for (let j=0; j<n; j++) {
-        if (j !== idx) {
-            let [x1, x2] = infos[j];
-            
-            // 기준 선분보다 좁게 들어오는 경우거나, 넓게 들어오는 경우 겹쳐진다
-            if (baseX1 < x1 && baseX2 > x2) {
-                flag = true;
-            } else if (baseX1 > x1 && baseX2 < x2) {
-                flag = true;
-            }
+        // 기준선인 경우 제외한다
+        if (j === i) {
+            continue;
+        }
+
+        // 기준선보다 더 넓게 들어오는경우 또는 더 좁게 들어오는 경우 겹친다
+        if ((infos[i][0] <= infos[j][0] && infos[i][1] >= infos[j][1]) || (infos[i][0] >= infos[j][0] && infos[i][1] <= infos[j][1])) {
+            flag = true;
+            break;
         }
     }
 
-    // 다른 선분과 겹쳐지는 경우가 존재하지 않으면 세어준다
     if (!flag) rlt++;
-    idx++;
 }
 
 console.log(rlt);
