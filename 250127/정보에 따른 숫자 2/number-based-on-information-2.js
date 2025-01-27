5e5a293b22faac71e7a0ab990681b5048eb304ed
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-27 20:14:05 +0900

    Title: 정보에 따른 숫자 2, Time: 198ms, Memory: 16MB, Status: Passed - CodeTree

--- 250127/정보에 따른 숫자 2/number-based-on-information-2.js
+++ 250127/정보에 따른 숫자 2/number-based-on-information-2.js
@@ -4,33 +4,23 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [t, a, b] = input[0].split(' ').map(Number);
 const infos = Array.from({length: t}, (_, i) => input[i+1].split(' '));
 
-let dict = {};
-for (let info of infos) {
-    let c = info[0], x = Number(info[1]);
-
-    if (!dict[c]) {
-        dict[c] = [x];
-    } else {
-        dict[c].push(x);
-    }
-}
-
 let rlt = 0;
-for (let numb=a; numb<=b; numb++) {
-    // S, N까지의 거리 중 최솟값들을 구해본다
-    let dist1 = Number.MAX_SAFE_INTEGER, dist2 = Number.MAX_SAFE_INTEGER;
-    for (let [k, v] of Object.entries(dict)) {
-        for (let i=0; i<v.length; i++) {
-            if (k === 'S') {
-                dist1 = Math.min(Math.abs(v[i]-numb), dist1);
-            } else {
-                dist2 = Math.min(Math.abs(v[i]-numb), dist2);
-            }
-        }
-    }
 
-    // s까지의 거리가 N까지의 거리보다 같거나 작으면 특별한 위치이다
-    if (dist1 <= dist2) {
+// a이상 b이하 숫자 하나씩 판단해본다
+for (let numb=a; numb<=b; numb++) {
+    let distS = Number.MAX_SAFE_INTEGER;
+    let distN = Number.MAX_SAFE_INTEGER;
+
+    // 받은 데이터들을 하나씩 확인해본다
+    infos.forEach(([c, x]) => {
+        if (c === 'N') {
+            distN = Math.min(distN, Math.abs(x-numb));
+        } else {
+            distS = Math.min(distS, Math.abs(x-numb));
+        }
+    })
+
+    if (distS <= distN) {
         rlt++;
     }
 }
