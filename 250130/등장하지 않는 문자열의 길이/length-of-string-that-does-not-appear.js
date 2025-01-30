aa97da9af6a4e362dc287ae7e683173e2763a689
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-30 14:27:44 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 167ms, Memory: 15MB, Status: Passed - CodeTree

--- 250130/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.js
+++ 250130/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.js
@@ -4,22 +4,31 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const str = input[1];
 
-let cnt = 0; // 문자열 길이 갱신해줄 변수
+let ans = 0;
 
-for (let i=0; i<n; i++) {
-    for (let j=i; j<n; j++) {
-        let checkStr = str.slice(i, j+1);
-        
-        let k = str.slice(j+1); // 나머지 문자열
+for (let i=1; i<n; i++) {
+    let duplicate = [];
+    let flag = true;
 
-        // 나머지 문자열에 포함되어 있으면 지나가기
-        if (k.includes(checkStr)) {
-            continue;
-        } else {
-            cnt = Math.max(checkStr.length, cnt);
+    for (let j=0; j<n-i+1; j++) {
+        // 길이 i인 문자열 추출한다
+        let k = str.slice(j, j+i)
+
+        // 그 문자열의 중복 문자열이 이미 들어있으면 더 확인할 필요가 없다
+        if (duplicate.includes(k)) {
+            flag = false;
             break;
         }
+
+        // 중복문자열이 안들어가있으면 넣어주기
+        duplicate.push(k);
+    }
+
+    // 중복문자열이 없었다면 i 길이를 ans로 갱신한다
+    if (flag) {
+        ans = i;
+        break;
     }
 }
 
-console.log(cnt);
+console.log(ans);
