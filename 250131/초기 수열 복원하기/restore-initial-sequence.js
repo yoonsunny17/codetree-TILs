5d79d1a7f210e9853bd87e4749eac98a31a019c9
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-31 23:52:39 +0900

    Title: 초기 수열 복원하기, Time: 173ms, Memory: 15MB, Status: Passed - CodeTree

--- 250131/초기 수열 복원하기/restore-initial-sequence.js
+++ 250131/초기 수열 복원하기/restore-initial-sequence.js
@@ -4,24 +4,27 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const infos = input[1].split(' ').map(Number);
 
-// 1부터 n의 숫자 중 하나를 선택해서, 원래 수열의 idx=0에 해당하는 숫자라고 가정해본다
-for (let i=1; i<=n; i++) {
-    let origin = [i];
-
+const checkNumbs = (arr) => {
     for (let info of infos) {
-        // 인접한 숫자의 정보를 통해 다음 요소를 계산한다
-        let calc = info - origin[origin.length - 1];
+        let calc = info - arr[arr.length-1];
 
-        // 해당 숫자가 범위를 벗어나면 무효
+        // 범위를 벗어나는 경우, 빈배열을 반환한다
         if (calc > n || calc < 1) {
-            break;
-        } else {
-            origin.push(info - origin[origin.length-1]);
+            return [];
         }
+
+        arr.push(calc);
     }
 
-    // 완성된 배열 안의 숫자가 중복되지 않아야 한다
-    if (new Set(origin).size === origin.length) {
+    return arr;
+}
+
+// 1부터 n의 숫자 중 하나를 선택해서, 원래 수열의 idx=0에 해당하는 숫자라고 가정해본다
+for (let i=1; i<=n; i++) {
+    let origin = checkNumbs([i]);
+    
+    // 완성된 수열에 중복된 숫자가 있는지 확인한다
+    if (new Set(origin).size === n) {
         console.log(origin.join(' '));
         break;
     }
