fca38a0b4723010bbfe5ed75ec2c0eac61e8cbba
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-01-31 22:12:07 +0900

    Title: 훌륭한 점프, Time: 171ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250131/훌륭한 점프/great-jump.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, k] = input[0].split(' ').map(Number);
+const numbs = input[1].split(' ').map(Number);
+
+const isPossible = (numb) => {
+    const availableIndices = [];
+    numbs.forEach((val, idx) => {
+        if (numb >= val) {
+            availableIndices.push(idx);
+        }
+    })
+
+    // 각 인덱스의 거리가 k 이하인지 확인
+    for (let i=1; i<availableIndices.length; i++) {
+        if (availableIndices[i] - availableIndices[i-1] > k) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+let minMax = n; // 최댓값들 중 최솟값
+for (let a=n; a>=Math.max(numbs[0], numbs[n-1]); a--) {
+    // k 이내에 들어올 수 있다면, minMax 갱신해줘
+    if (isPossible(a)) {
+        minMax = Math.min(minMax, a);
+    }
+}
+
+console.log(minMax);
