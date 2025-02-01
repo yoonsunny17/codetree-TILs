d5ea8c8b5f3a93f9a9546014b6b69690451a0767
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-02-01 20:32:32 +0900

    Title: 두 선분, Time: 175ms, Memory: 15MB, Status: Passed - CodeTree

--- 250201/두 선분/two-lines.js
+++ 250201/두 선분/two-lines.js
@@ -4,10 +4,10 @@ const input = fs.readFileSync(0).toString().trim().split(' ').map(Number);
 const check = (input) => {
     const [x1, x2, x3, x4] = input;
     
-    if (x1 <= x4 || x2 >= x3) {
-        return "intersecting"
-    } else {
+    if (x2 < x3 || x1 > x4) {
         return "nonintersecting"
+    } else {
+        return "intersecting"
     }
 }
 
