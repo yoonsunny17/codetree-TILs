7ed5b6f04705f66df7ab05d5a08742158a32348f
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-02-01 20:50:01 +0900

    Title: 구역 청소, Time: 165ms, Memory: 15MB, Status: Passed - CodeTree

--- 250201/구역 청소/district-cleaning.js
+++ 250201/구역 청소/district-cleaning.js
@@ -4,7 +4,12 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [a, b] = input[0].split(' ').map(Number);
 const [c, d] = input[1].split(' ').map(Number);
 
-const maxVal = Math.max(a, b, c, d);
-const minVal = Math.min(a, b, c, d);
-
-console.log(maxVal - minVal);
+if (b < c || d < a) {
+    // 안겹치는 경우 >> b-a + d-c
+    console.log(b - a + d - c);
+} else {
+    // 겹치는 경우 >> max-min
+    const maxV = Math.max(a, b, c, d);
+    const minV = Math.min(a, b, c, d);
+    console.log(maxV - minV);
+}
