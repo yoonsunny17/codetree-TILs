7e6d88da9245f902befceadd1ddd980972997cd4
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-02-05 23:17:59 +0900

    Title: 최소 와이파이 수, Time: 178ms, Memory: 15MB, Status: Passed - CodeTree

--- 250205/최소 와이파이 수/minimum-number-of-wifi.js
+++ 250205/최소 와이파이 수/minimum-number-of-wifi.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [n, m] = input[0].split(' ').map(Number);
-const infos = input[1].split(' ').map(Number);
+const infos = input[1].trim().split(' ').map(Number);
 
 // 사람이 사는 위치라면, 그 위치로부터 m만큼 오른쪽 떨어진 곳에 설치한다
 // 설치한 곳에서 2m 떨어진 곳에서 다시 탐색한다
@@ -11,7 +11,7 @@ let cnt = 0, idx = 0;
 while (idx < n) {
     if (infos[idx] === 1) {
         cnt++;
-        idx += 2 * m + idx;
+        idx += 2 * m + 1;
     } else {
         idx++;
     }
