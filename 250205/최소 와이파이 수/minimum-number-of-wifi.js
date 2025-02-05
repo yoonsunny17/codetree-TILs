95a4a7dc2f97c3468eb21c362f7a6b7ff15d6d62
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-02-05 23:16:41 +0900

    Title: 최소 와이파이 수, Time: 5008ms, Memory: 14MB, Status: Time Limit Exceed - CodeTree

--- 250205/최소 와이파이 수/minimum-number-of-wifi.js
+++ 250205/최소 와이파이 수/minimum-number-of-wifi.js
@@ -1,36 +1,20 @@
-// 첫 설치는 m에 한다 (그러면 idx=0인 부분부터, 2m 위치까지 커버 가능)
-// 그 다음 설치는 현재 와이파이 설치된 곳으로부터 2m + 1 인 곳에 한다
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [n, m] = input[0].split(' ').map(Number);
 const infos = input[1].split(' ').map(Number);
 
-if (!infos.includes(1)) {
-    console.log(0);
-    return;
-}
+// 사람이 사는 위치라면, 그 위치로부터 m만큼 오른쪽 떨어진 곳에 설치한다
+// 설치한 곳에서 2m 떨어진 곳에서 다시 탐색한다
 
-if (infos.includes(1) && n <= m) {
-    console.log(1);
-    return;
-}
-
-idx = m;
-cnt = 1;
-while (true) {
-    idx += 2 * m + 1;
-
-    if (idx >= n) {
-        break;
+let cnt = 0, idx = 0;
+while (idx < n) {
+    if (infos[idx] === 1) {
+        cnt++;
+        idx += 2 * m + idx;
+    } else {
+        idx++;
     }
-
-    // 근데 와이파이 두려는 곳에 사람이 살지 않으면, 굳이 설치할 필요가 없다
-    if (infos[idx] === 0) {
-        idx += 1;
-    }
-
-    cnt++;
 }
 
-console.log(cnt)
+console.log(cnt);
