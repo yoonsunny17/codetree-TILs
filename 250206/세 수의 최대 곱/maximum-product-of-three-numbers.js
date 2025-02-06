f88e3160ef42adb70c500edf92deeda76bfdc937
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-02-06 22:41:35 +0900

    Title: 세 수의 최대 곱, Time: 176ms, Memory: 15MB, Status: Passed - CodeTree

--- 250206/세 수의 최대 곱/maximum-product-of-three-numbers.js
+++ 250206/세 수의 최대 곱/maximum-product-of-three-numbers.js
@@ -4,20 +4,43 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input.shift());
 const numbs = input[0].split(' ').map(Number);
 
-// 음수 세개 => 음수
-// 음수 두개, 양수 하나 => 양수
-// 음수 하나, 양수 두개 => 음수
-// 양수 세개 => 양수
-
-let maxVal = Number.MIN_SAFE_INTEGER;
-
-// 세개의 숫자를 골라서 곱한 뒤, 최댓값을 갱신한다
-for (let i=0; i<n; i++) {
-    for (let j=i+1; j<n; j++) {
-        for (let k=j+1; k<n; k++) {
-            maxVal = Math.max(maxVal, numbs[i] * numbs[j] * numbs[k]);
-        }
-    }
+// 애초에 세개만 주어졌다면 세개의 숫자 곱해서 반환한다
+if (numbs.length === 3) {
+    console.log(numbs[0] * numbs[1] * numbs[2]);
+    return;
 }
 
-console.log(maxVal);
+// 양수 배열과 음수 배열을 나누고, 양수는 내림차순, 음수는 오름차순으로 으로 정렬한다
+const positive = numbs.filter((n) => n >= 0).sort((a, b) => b - a);
+const negative = numbs.filter((n) => n < 0).sort((a, b) => a - b);
+
+// 음수 배열이 비어있는 경우 >> 양수 배열에서 세개 뽑아서 곱한다
+// 음수 배열에 하나만 있는 경우 >> 양수 배열에서 세개 뽑아서 곱한다
+if (negative.length <= 1) {
+    console.log(positive[0] * positive[1] * positive[2]);
+    return;
+}
+
+// 양수 배열이 비어있는 경우 >> 음수 배열에서 뒤에서 세개 뽑아서 곱한다
+if (!positive.length) {
+    console.log(negative[negative.length-1] * negative[negative.length-2] * negative[negative.length-3]);
+    return;
+}
+
+// 양수 배열에 하나만 있는 경우 >> 음수 배열에서 앞에서 두개, 양수 배열 하나를 곱한다
+if (positive.length === 1) {
+    console.log(positive[0] * negative[0] * negative[1]);
+    return;
+}
+
+// 양수 배열에 두개만 있는 경우 >> 양수 배열에서 하나, 음수 배열에서 두개를 곱한다
+if (positive.length === 2) {
+    console.log(positive[0] * negative[0] * negative[1]);
+    return;
+}
+
+// 나머지 경우 >> 양수 세개 vs 음수두개 양수하나 값 비교해서 최댓값을 뽑는다
+let val1 = positive[0] * positive[1] * positive[2];
+let val2 = positive[0] * negative[0] * negative[1];
+
+console.log(Math.max(val1, val2));
