6064bfc59e253721692b49554f9025123f0aea1f
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-02-09 20:15:22 +0900

    Title: 컨베이어 벨트, Time: 191ms, Memory: 15MB, Status: Passed - CodeTree

--- 250209/컨베이어 벨트/conveyor-belt.js
+++ 250209/컨베이어 벨트/conveyor-belt.js
@@ -1,37 +1,18 @@
-// 윗줄: (0, n-1)을 tmp1에 담고, 나머지 수들을 왼 -> 오 밀어준다
-// 아랫줄: (1, 0)을 tmp2에 담고, 나머지 수들을 오 -> 왼 밀어준다
-// tmp1을 (1, n-1)에 넣어주고, tmp2를 (0, 0)에 넣어준다
-
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [n, t] = input[0].split(' ').map(Number);
 const numbs = [];
-numbs.push(input[1].split(' ').map(Number));
-numbs.push(input[2].split(' ').map(Number).reverse());
-
-let time = 0;
-while (time !== t) {
-    // 한바퀴 돌리고 1초 더한다
-    let tmp_up = numbs[0][n-1]; // 윗줄은 가장 오른쪽 원소를 저장한다
-    let tmp_down = numbs[1][0]; // 아랫줄은 가장 왼쪽 원소를 저장한다
-
-    for (let i=n-1; i>0; i--) {
-        numbs[0][i] = numbs[0][i-1]; // 윗줄을 왼 > 오 방향으로 한칸씩 밀어준다
-    }
-    for (let i=1; i<=n-1; i++) {
-        numbs[1][i-1] = numbs[1][i]; // 아랫줄을 오 > 왼 방향으로 한칸씩 밀어준다
-    }
-
-    numbs[0][0] = tmp_down;
-    numbs[1][n-1] = tmp_up;
-    time++;
+for (let i=1; i<=2; i++) {
+    numbs.push(...input[i].trim().split(' ').map(Number));
 }
 
-for (let i=0; i<2; i++) {
-    if (i === 1) {
-        console.log(numbs[i].reverse().join(' '));
-    } else {
-        console.log(numbs[i].join(' '));
-    }
+// t초동안 반복한다
+for (let time=0; time<t; time++) {
+    // 가장 오른쪽의 숫자를 가장 왼쪽에 넣어준다
+    numbs.unshift(numbs.pop());
 }
+
+// n개씩 끊어서 출력한다
+console.log(numbs.slice(0, n).join(' '));
+console.log(numbs.slice(n, 2*n).join(' '));
