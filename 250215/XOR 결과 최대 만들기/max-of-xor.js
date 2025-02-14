97eabfdfe9d05db44f8572c6345e09efccdf7856
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-02-15 00:21:54 +0900

    Title: XOR 결과 최대 만들기, Time: 210ms, Memory: 16MB, Status: Passed - CodeTree

--- 250215/XOR 결과 최대 만들기/max-of-xor.js
+++ 250215/XOR 결과 최대 만들기/max-of-xor.js
@@ -3,39 +3,30 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [n, m] = input[0].split(' ').map(Number);
 const numbs = input[1].split(' ').map(Number);
-numbs.sort((a, b) => a - b); // 오름차순 정렬
 
 let maxVal = 0;
-let selected = []; // m개의 숫자 선택해서 넣을 리스트
+let selected = []; // m개의 숫자를 선택한 리스트
 
-// 선택한 수열에 대해, xor 연산을 해본다
+// xor 연산 수행
 function calcXOR(selected) {
-    return selected.reduce((acc, curr) => acc ^ Number(curr.toString(2)), 0);
+    return selected.reduce((acc, curr) => acc ^ curr, 0);
 }
 
-// m개의 숫자를 선택한다
-function chooseNumber(cnt) {
-    if (cnt === m + 1) {
-        // m개의 숫자를 모두 선택했다면, xor 계산 한다
-        maxVal = Math.max(maxVal, parseInt(calcXOR(selected), 2));
+// m개의 숫자를 선택하기
+function chooseNumber(idx, cnt) {
+    if (cnt === m) {
+        // m개를 선택했을 때 > 최댓값 갱신한다
+        maxVal = Math.max(maxVal, calcXOR(selected));
+        return;
     }
 
-    for (let i=0; i<n; i++) {
-        // 아직 선택한 숫자가 없다면 그냥 넣어도 된다
-        if (selected.length === 0) {
-            selected.push(numbs[i]);
-            chooseNumber(cnt+1);
-            selected.pop();
-        } else {
-            // 선택한 숫자가 있으면, 그 숫자보다 큰 숫자부터 넣을 수 있다
-            if (numbs[i] > selected[selected.length - 1]) {
-                selected.push(numbs[i]);
-                chooseNumber(cnt+1);
-                selected.pop();
-            }
-        }
+    // 현재 인덱스부터 하나씩 탐색 진행한다
+    for (let i=idx; i<n; i++) {
+        selected.push(numbs[i]);
+        chooseNumber(i+1, cnt+1);
+        selected.pop();
     }
 }
 
-chooseNumber(1);
+chooseNumber(0, 0);
 console.log(maxVal);
