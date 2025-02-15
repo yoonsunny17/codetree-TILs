b87be7cc25fd3e738f621542626d912e656fc096
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-02-15 22:14:19 +0900

    Title: 떨어지는 1자 블록, Time: 182ms, Memory: 15MB, Status: Passed - CodeTree

--- 250215/떨어지는 1자 블록/falling-horizontal-block.js
+++ 250215/떨어지는 1자 블록/falling-horizontal-block.js
@@ -2,12 +2,14 @@ const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [n, m, k] = input[0].split(' ').map(Number);
-const matrix = Array.from({length: n}, (_, i) => input[i+1].split(' ').map(Number));
+const matrix = Array.from({length: n}, (_, i) => input[i+1].trim().split(' ').map(Number));
 
 // 범위 안에 1이 있다면 블럭이 놓여질 수 없다
 const checkBlock = (row) => {
-    if (matrix[row].slice(k-1, k+m-1).includes(1)) {
-        return false;
+    for (let j=k-1; j<k+m-1; j++) {
+        if (matrix[row][j] === 1) {
+            return false;
+        }
     }
     return true;
 }
