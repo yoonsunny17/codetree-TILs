4fe5df4ac1e1b993badf888bd1cf3a8650431238
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-02-17 00:48:19 +0900

    Title: 외판원 순회, Time: 241ms, Memory: 15MB, Status: Passed - CodeTree

--- 250217/외판원 순회/traveling-salesman-problem.js
+++ 250217/외판원 순회/traveling-salesman-problem.js
@@ -7,24 +7,28 @@ const matrix = Array.from({length: n}, (_, i) => input[i+1].trim().split(' ').ma
 const visited = new Array(n).fill(false);
 
 let minVal = Number.MAX_SAFE_INTEGER;
-const chooseNumber = (row, val) => {
-    // n개의 행을 모두 이동했다면 최솟값 갱신해준다
-    if (row === n) {
+const chooseNumber = (check, row, val) => {
+    // n개의 행을 모두 이동했다면 최솟값을 갱신해준다
+    if (check === n) {
         minVal = Math.min(minVal, val);
         return;
     }
 
     for (let j=0; j<n; j++) {
-        // 같은 번호로 이동하는 경우는 없다
-        // 이미 방문한 곳이면 건너뛴다
+        // 1. 같은 번호로 이동하는 경우는 없다
+        // 2. 이미 방문한 곳은 건너뛴다
+        // 3. 0인곳은 갈 수 없다
+        // 4. 마지막 체크 행이 아닌데 0이면 안된다
         if (j === row) continue;
         if (visited[j]) continue;
+        if (matrix[row][j] === 0) continue;
+        if (check < n-1 && j === 0) continue;
 
         visited[j] = true;
-        chooseNumber(row+1, val+matrix[row][j]);
+        chooseNumber(check+1, j, val+matrix[row][j]);
         visited[j] = false;
     }
 }
 
-chooseNumber(0, 0);
+chooseNumber(0, 0, 0);
 console.log(minVal);
