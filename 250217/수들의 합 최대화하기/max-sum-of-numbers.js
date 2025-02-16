664f2e7b6d0a4033d453175d972cbe93f7d6a651
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-02-17 00:14:34 +0900

    Title: 수들의 합 최대화하기, Time: 186ms, Memory: 15MB, Status: Passed - CodeTree

--- 250217/수들의 합 최대화하기/max-sum-of-numbers.js
+++ 250217/수들의 합 최대화하기/max-sum-of-numbers.js
@@ -4,35 +4,27 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const n = Number(input[0]);
 const matrix = Array.from({length: n}, (_, i) => input[i+1].trim().split(' ').map(Number));
 
-// 행과 열에 대해 방문 체크 리스트를 따로 만들어준다
-const visited_row = new Array(n).fill(false);
-const visited_col = new Array(n).fill(false);
+const visited = new Array(n).fill(false);
 
-let maxVal = 0;
-let val = 0;
-const chooseNumber = (curr) => {
-    if (curr === n+1) {
+let maxVal = -1;
+
+const chooseNumber = (row, val) => {
+    // n번째 열까지 모두 확인했다면 최댓값 갱신해준다
+    if (row === n) {
         maxVal = Math.max(maxVal, val);
         return;
     }
 
-    for (let i=0; i<n; i++) {
-        for (let j=0; j<n; j++) {
-            // 만약 i또는 j가 하나라도 방문 처리가 되어 있다면 건너뛴다
-            if (visited_row[i] || visited_col[j]) continue;
-
-            visited_row[i] = true;
-            visited_col[j] = true;
-            val += matrix[i][j];
-
-            chooseNumber(curr+1);
-
-            visited_row[i] = false;
-            visited_col[j] = false;
-            val -= matrix[i][j];
-        }
+    // 각 row마다 검사해본다
+    for (let j=0; j<n; j++) {
+        // j위치에 이미 방문한 적 있다면 건너뛴다
+        if (visited[j]) continue;
+        
+        visited[j] = true;
+        chooseNumber(row+1, val+matrix[row][j]);
+        visited[j] = false;
     }
 }
 
-chooseNumber(1);
+chooseNumber(0, 0);
 console.log(maxVal);
