9973810330e57da6a94b2c9cde30453597dcd4f7
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-02-21 15:35:21 +0900

    Title: 돌 잘 치우기, Time: 5061ms, Memory: 39MB, Status: Time Limit Exceed - CodeTree

--- 250221/돌 잘 치우기/clear-stones-well.js
+++ 250221/돌 잘 치우기/clear-stones-well.js
@@ -14,15 +14,29 @@ const inRange = (r, c) => {
 const removeStones = (matrix, cnt) => {
     let grid = matrix.map((row) => [...row]); // deep copy
 
+    // m개의 돌을 치웠으면, 최대 이동 가능한 곳의 개수를 센다 (한 지점당 한번씩이 아니라, 한번에 몇군데 갈 수 있는지를 계산한다)
     if (cnt === m) {
+        let visited = Array.from({length: n}, () => Array.from({length: n}, () => false));
         // m개의 돌을 치웠을 때, bfs 탐색을 돌려본다
         for (let info of infos) {
             const [row, col] = info.map((v) => Number(v)-1);
-            maxVal = Math.max(maxVal, bfs(row, col, grid));
-            return;
+            bfs(row, col, grid, visited);
         }
+        
+        // 총 방문한 곳의 개수를 세어주고, 최댓값 갱신한다
+        let total = 0;
+        for (let i=0; i<n; i++) {
+            for (let j=0; j<n; j++) {
+                if (visited[i][j] === true) {
+                    total++;
+                }
+            }
+        }
+
+        maxVal = Math.max(maxVal, total);
     }
 
+    // 아직 m개의 돌을 치우는 중
     for (let i=0; i<n; i++) {
         for (let j=0; j<n; j++) {
             if (matrix[i][j] === 1) {
@@ -34,8 +48,7 @@ const removeStones = (matrix, cnt) => {
     }
 }
 
-const bfs = (r, c, grid) => {
-    let visited = Array.from({length: n}, () => Array.from({length: n}, () => false));
+const bfs = (r, c, grid, visited) => {
     let q = [];
 
     visited[r][c] = true;
@@ -44,8 +57,6 @@ const bfs = (r, c, grid) => {
     // 상 하 좌 우
     let dr = [-1, 1, 0, 0];
     let dc = [0, 0, -1, 1];
-
-    let cnt = 1;
 
     while (q.length) {
         let [row, col] = q.shift();
@@ -58,12 +69,9 @@ const bfs = (r, c, grid) => {
             if (inRange(nr, nc) && !visited[nr][nc] && grid[nr][nc] === 0) {
                 visited[nr][nc] = true;
                 q.push([nr, nc]);
-                cnt++;
             }
         }
     }
-
-    return cnt;
 }
 
 removeStones(matrix, 0);
