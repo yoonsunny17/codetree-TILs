23ca248a4b800c42afcc0448245d427ea82d78a7
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-02-23 12:27:23 +0900

    Title: 비를 피하기, Time: 179ms, Memory: 15MB, Status: Passed - CodeTree

--- 250223/비를 피하기/stay-out-of-rain.js
+++ 250223/비를 피하기/stay-out-of-rain.js
@@ -1,58 +1,76 @@
+// 가중치가 동일한 그래프에서 시작점이 여러개인 상태로 BFS 돌리는 경우
 const fs = require('fs');
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const [n, h, m] = input[0].split(' ').map(Number);
-const matrix = Array.from({length: n}, (_, i) => input[i+1].trim().split(' ').map(Number));
+const matrix = Array.from({length: n}, (_, i) => input[i+1].split(' ').map(Number));
+
+// 3인 위치들을 저장한다
+const start_pos = [];
+for (let i=0; i<n; i++) {
+    for (let j=0; j<n; j++) {
+        if (matrix[i][j] === 3) start_pos.push([i, j]);
+    }
+}
+
+const q = [];
+const visited = Array.from({length: n}, () => Array(n).fill(false));
+const step = Array.from({length: n}, () => Array(n).fill(0));
 
 const inRange = (r, c) => r >= 0 && r < n && c >= 0 && c < n;
 
-const bfs = (r, c) => {
-    let visited = Array.from({ length: n }, () => Array(n).fill(false));
-    let q = [[r, c, 0]];
-    visited[r][c] = true;
-    
-    // 상 하 좌 우
-    let dr = [-1, 1, 0, 0];
-    let dc = [0, 0, -1, 1];
+// 범위 내, 미방문, 1이 아닌 곳은 탐색 가능하다
+const canGo = (r, c) => {
+    return inRange(r, c) && !visited[r][c] && matrix[r][c] !== 1;
+} 
 
-    let flag = false;
+// q에 새로운 위치 추가하고 방문 여부 표시한다
+const push = (nr, nc, cnt) => {
+    q.push([nr, nc]);
+    visited[nr][nc] = true;
+    step[nr][nc] = cnt;
+}
 
+const bfs = () => {
     while (q.length) {
-        let [row, col, cnt] = q.shift();
-        for (let i=0; i<4; i++) {
-            let nr = row + dr[i];
-            let nc = col + dc[i];
+        const [row, col] = q.shift();
 
-            // 범위 내, 미방문, 1이 아니라면 탐색 가능하다
-            if (inRange(nr, nc) && !visited[nr][nc] && matrix[nr][nc] !== 1) {
-                // 근데 만약 3인 곳이라면 이동 완료
-                if (matrix[nr][nc] === 3) {
-                    flag = true; // 도착했음을 flag로 표시
-                    return cnt+1;
-                } else {
-                    visited[nr][nc] = true;
-                    q.push([nr, nc, cnt+1]);
-                }
+        // 상 하 좌 우
+        const dr = [-1, 1, 0, 0];
+        const dc = [0, 0, -1, 1];
+
+        for (let i=0; i<4; i++) {
+            const nr = row + dr[i];
+            const nc = col + dc[i];
+
+            // 탐색 가능하면 q에 넣어준다
+            if (canGo(nr, nc)) {
+                push(nr, nc, step[row][col]+1);
             }
         }
     }
-
-    // 3으로 이동이 불가한 경우
-    if (!flag) {
-        return -1;
-    }
 }
 
-// 사람이 있는 위치 대상으로 bfs를 돌려본다
-let rlt = Array.from({length: n}, () => Array(n).fill(0)); // 정답 저장할 2차원 배열
+// 3인 곳들에 대해 q에 위치 추가하고 방문 여부 표시한다
+start_pos.forEach(([r, c]) => push(r, c, 0));
+bfs();
+
+let rlt = '';
 for (let i=0; i<n; i++) {
     for (let j=0; j<n; j++) {
-        if (matrix[i][j] === 2) {
-            rlt[i][j] = bfs(i, j);
+        // 사람이 있는 위치 제외 모두 0이다
+        if (matrix[i][j] !== 2) {
+            rlt += '0 '
+        } else {
+            // 사람이 있는데 방문한 적이 없으면 -1
+            if (!visited[i][j]) {
+                rlt += '-1 ';
+            } else {
+                rlt += `${step[i][j]} `;
+            }
         }
     }
+    rlt += "\n";
 }
 
-for (let row of rlt) {
-    console.log(row.join(' '));
-}
+console.log(rlt);
