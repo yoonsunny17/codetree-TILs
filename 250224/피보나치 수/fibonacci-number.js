933015d2c81c51e7132507bd7e80f1b1098392fe
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-02-24 12:40:01 +0900

    Title: 피보나치 수, Time: 163ms, Memory: 15MB, Status: Passed - CodeTree

--- 250224/피보나치 수/fibonacci-number.js
+++ 250224/피보나치 수/fibonacci-number.js
@@ -1,23 +1,14 @@
 const fs = require('fs');
 const n = Number(fs.readFileSync(0).toString().trim());
 
-const memo = Array.from({length: n+1}, () => -1);
+// tabulation
+let dp = Array(n).fill(0);
 
-const fibo = (n) => {
-    // 이미 n번째 값이 구해져있다면 그 값을 리턴한다
-    if (memo[n] !== -1) {
-        return memo[n];
-    }
+dp[0] = 1;
+dp[1] = 1;
 
-    if (n <= 2) {
-        // 첫번째 원소와 두번재 원소는 1이다
-        memo[n] = 1;
-    } else {
-        // 조건 만족할 때 까지 더해나간다
-        memo[n] = fibo(n-1) + fibo(n-2);
-    }
-
-    return memo[n];
+for (let i=2; i<n; i++) {
+    dp[i] = dp[i-1] + dp[i-2];
 }
 
-console.log(fibo(n));
+console.log(dp[n-1]);
