79f158fce49081fea321316079d9e60305b79b15
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-02-25 20:05:38 +0900

    Title: 사각형 채우기 3, Time: 166ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250225/사각형 채우기 3/rectangle-fill-3.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const n = Number(fs.readFileSync(0).toString().trim());
+
+function countWays(n) {
+    if (n === 0) return 1;
+    if (n === 1) return 2;
+    if (n === 2) return 7;
+
+    let dp = new Array(n + 1).fill(0);
+    dp[0] = 1;
+    dp[1] = 2;
+    dp[2] = 7;
+
+    for (let i=3; i<=n; i++) {
+        dp[i] = (3 * dp[i-1] + dp[i-2] - dp[i-3]) % 1000000007;
+        if (dp[i] < 0) dp[i] += 1000000007;
+    }
+    
+    return dp[n];
+}
+
+console.log(countWays(n));
