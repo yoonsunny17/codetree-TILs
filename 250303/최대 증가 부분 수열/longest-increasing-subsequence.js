96b41f9536a530a96a51feaa8b626c45d29e76d3
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-03-03 21:42:29 +0900

    Title: 최대 증가 부분 수열, Time: 1607ms, Memory: 18MB, Status: Passed - CodeTree

--- 250303/최대 증가 부분 수열/longest-increasing-subsequence.js
+++ 250303/최대 증가 부분 수열/longest-increasing-subsequence.js
@@ -3,18 +3,28 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = Number(input.shift());
 const numbs = input[0].split(' ').map(Number);
-const dp = Array.from({length: n}, () => -1);
-dp[0] = 1;
 
-for (let i=1; i<n; i++) {
-    for (let j=0; j<i; j++) {
-        if (dp[j] === -1) continue;
+let maxVal = -1;
 
-        // 조건을 만족한다면 (현재가 이전 숫자보다 더 큰 숫자라면) dp 갱신해준다
-        if (numbs[j] < numbs[i]) {
-            dp[i] = Math.max(dp[i], dp[j]+1);
+// 각 인덱스를 수열의 시작점으로 잡아본다
+for (let i=0; i<n; i++) {
+    const dp = Array.from({length: n}, () => -1);
+    dp[i] = 1;
+
+    for (let i=1; i<n; i++) {
+        for (let j=0; j<i; j++) {
+            if (dp[j] === -1) continue;
+            
+            // 조건을 만족한다면, dp를 갱신해준다
+            if (numbs[j] < numbs[i]) {
+                dp[i] = Math.max(dp[i], dp[j]+1);
+            }
         }
     }
+
+    // 해당 수열에서 나올 수 있는 최대 길이를 뽑고, 최종값을 갱신한다
+    let maxCnt = Math.max(...dp);
+    maxVal = Math.max(maxVal, maxCnt);
 }
 
-console.log(Math.max(...dp));
+console.log(maxVal);
