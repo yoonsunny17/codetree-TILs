f0f04440e2fb3b5a3e62af3364f9b95623b1c93a
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-03-05 17:30:41 +0900

    Title: 기울어진 직사각형, Time: 207ms, Memory: 17MB, Status: Passed - CodeTree

--- 250305/기울어진 직사각형/slanted-rectangle.js
+++ 250305/기울어진 직사각형/slanted-rectangle.js
@@ -1,58 +1,49 @@
 const fs = require('fs');
-const input = fs.readFileSync(0).toString().split('\n');
+const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = Number(input[0]);
 const matrix = Array.from({length: n}, (_, i) => input[i+1].trim().split(' ').map(Number));
 
-// 1 2 3 4 (진행방향 순서대로)
+// 1, 2, 3, 4
 const dr = [-1, -1, 1, 1];
 const dc = [1, -1, -1, 1];
 
 const inRange = (r, c) => r >= 0 && r < n && c >= 0 && c < n;
 
-const findSquare = (sr, sc) => {
-    const [or, oc] = [sr, sc];
-    let maxSum = 0;
+const findSquare = (r, c, k, l) => {
+    const moveNumb = [k, l, k, l];
     let sum = 0;
-    let dir = 0;
-    let check = Array(4).fill(0);
-     
-    while (dir < 4) {
-        let nr = sr + dr[dir];
-        let nc = sc + dc[dir];
 
-        // dir === 3인 경우 > 맨 처음 시작지점이라면 끝낸다
-        if (dir === 3 && nr === or && nc === oc) {
-            sum += matrix[sr][sc];
-            check[dir]++;
-            break;
-        }
+    // 4방향 탐색을 진행해본다
+    for (let d=0; d<4; d++) {
+        // (중요) 한 변당 k번 또는 l번을 반복해본다
+        for (let q=0; q<moveNumb[d]; q++) {
+            r += dr[d];
+            c += dc[d];
 
-        // 범위내에 존재한다면 갈 수 있다
-        if (inRange(nr, nc)) {
-            sum += matrix[sr][sc];
-            check[dir]++;
-            sr = nr;
-            sc = nc;
-        } else {
-            // 다음 지점이 범위 밖을 벗어난다면 방향을 바꿔준다
-            dir += 1;
-            continue;
+            // 직사각형이 만들어지지 않는다면 -1 리턴 끝
+            if (!inRange(r, c)) {
+                return -1;
+            }
+
+            sum += matrix[r][c];
         }
     }
 
-    // 네 방향에 대해서 모두 순환한 경우에만 출력한다
-    if (check.every((v) => v !== 0)) {
-        maxSum = Math.max(maxSum, sum);
-    }
-    return maxSum;
+    return sum;
 }
 
 let maxVal = -1;
-
+// (i, j)를 시작방향으로 1, 2, 3, 4번 dir 순서대로 탐색한다
+// 각 직사각형의 한 변의 길이가 [k, l, k, l] 이라고 생각하고
+// 해당 범위 내에서 돌려본다
 for (let i=0; i<n; i++) {
     for (let j=0; j<n; j++) {
-        maxVal = Math.max(maxVal, findSquare(i, j));
+        for (let k=1; k<n; k++) {
+            for (let l=1; l<n; l++) {
+                maxVal = Math.max(maxVal, findSquare(i, j, k, l));
+            }
+        }
     }
 }
 
