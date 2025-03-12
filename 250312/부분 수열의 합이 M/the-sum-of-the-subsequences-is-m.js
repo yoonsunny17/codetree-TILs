d516f7ab2d9aeb5f8ecfde71fb104ff77040693a
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-03-12 21:15:19 +0900

    Title: 부분 수열의 합이 M, Time: 195ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250312/부분 수열의 합이 M/the-sum-of-the-subsequences-is-m.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const numbs = input[1].split(' ').map(Number);
+
+const INT_MAX = Number.MAX_SAFE_INTEGER;
+let dp = Array(m+1).fill(INT_MAX);
+dp[0] = 0;
+
+// 수열의 숫자를 차례대로 검사해본다
+for (let i=1; i<=n; i++) {
+    // dp는 역순으로 체크해본다
+    for (let j=m; j>=0; j--) {
+        // 만약에 현재의 숫자가 합 j보다 작거나 같은 경우 고민해볼 수 있다
+        if (j >= numbs[i]) {
+            // 근데 현재 합 j에서 해당 숫자를 뺀 값이 아닌 경우라면 건너뛴다
+            if (dp[j - numbs[i]] === INT_MAX) {
+                continue;
+            }
+
+            // 숫자 합 j는 현재의 값 또는, 새로 더했을 때의 숫자 개수와 비교해서 최소 길이로 갱신한다
+            dp[j] = Math.min(dp[j], dp[j - numbs[i]] + 1);
+        }
+    }
+}
+
+console.log(dp[m] === INT_MAX ? -1 : dp[m]);
