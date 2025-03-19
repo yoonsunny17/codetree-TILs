7f2536f4f05dba5a47df1644d9d8911b44b2f12a
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-03-19 22:59:29 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: 172ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250319/연속 부분 합의 최댓값 구하기/max-of-partial-sum.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const numbs = input[1].split(' ').map(Number);
+
+const INT_MIN = Number.MIN_SAFE_INTEGER;
+let dp = Array(n).fill(INT_MIN);
+dp[0] = numbs[0]; // 0번째 dp = 수열의 첫 번째 원소의 값 (초기화)
+
+// dp[i] = i번째 숫자까지 더한 부분수열 합 중 최댓값
+for (let i=1; i<n; i++) {
+    // i-1번째 부분수열 합에 i번째 숫자를 더한 값이 큰지, 아니면 i번째 숫자 자체만이 더 큰지 비교한다
+    dp[i] = Math.max(numbs[i], dp[i-1] + numbs[i]);
+}
+
+console.log(dp[n-1]);
