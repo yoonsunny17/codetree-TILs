8d92eb3f9b6ced728b0d47d4bb795ad07cba9b0d
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-03-24 22:48:49 +0900

    Title: 방향에 맞춰 최대로 움직이기, Time: 194ms, Memory: 15MB, Status: Passed - CodeTree

--- 250324/방향에 맞춰 최대로 움직이기/max-movements-with-direction.js
+++ 250324/방향에 맞춰 최대로 움직이기/max-movements-with-direction.js
@@ -9,18 +9,20 @@ const [sr, sc] = input[2*N+1].split(' ').map((v) => Number(v)-1);
 const dr = [-1, -1, 0, 1, 1, 1, 0, -1];
 const dc = [0, 1, 1, 1, 0, -1, -1, -1];
 
-let ans = -1; // 최대 이동 횟수 저장할 변수
+let ans = 0; // 최대 이동 횟수 저장할 변수
 
 const inRange = (r, c) => r >= 0 && r < N && c >= 0 && c < N;
 
 // 다음 탐색이 범위 내에 존재하고, 현재 탐색 지점보다 큰 숫자인 경우 움직일 수 있다
 const canMove = (r, c) => {
-    let dir = directions[r][c] - 1;
-    let nr = r + dr[dir];
-    let nc = c + dc[dir];
+    for (let i=1; i<=N; i++) {
+        let dir = directions[r][c] - 1;
+        let nr = r + dr[dir] * i;
+        let nc = c + dc[dir] * i;
 
-    if (inRange(nr, nc) && matrix[nr][nc] > matrix[r][c]) {
-        return true;
+        if (inRange(nr, nc) && matrix[nr][nc] > matrix[r][c]) {
+            return true;
+        }
     }
 
     return false;
@@ -45,5 +47,5 @@ const backtracking = (cnt, r, c) => {
     }
 }
 
-backtracking(1, sr, sc);
+backtracking(0, sr, sc);
 console.log(ans);
