3486e889d93753047919739f8b15a75575476b85
yoonsun_jun <97651006+yoonsunny17@users.noreply.github.com>
2025-04-12 14:53:59 +0900

    Title: Top K 숫자 구하기, Time: 178ms, Memory: 15MB, Status: Passed - Codetree

--- 250412/Top K 숫자 구하기/kth-number.js
+++ 250412/Top K 숫자 구하기/kth-number.js
@@ -4,5 +4,5 @@ const input = fs.readFileSync(0).toString().trim().split('\n');
 const [n, k] = input[0].trim().split(' ').map(Number);
 const numbs = input[1].trim().split(' ').map(Number);
 
-numbs.sort();
+numbs.sort((a, b) => a-b);
 console.log(numbs[k-1]);
